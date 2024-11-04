ad32cb2cf49490c96bf125834411b86829c36e8c
Praseesh <praseeshprasee@gmail.com>
2024-11-04 16:42:49 +0530

    cache_set_get_method

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import HttpResponse
 from django.core.cache import cache
 
-def cache_example_view(request):
+def cache_set_get_method(request):
     cache.set('orange', '100', timeout=60*1)
     cache.set('mango', 50, timeout=60)
     value = cache.get('orange') 
@@ -9,7 +9,6 @@ def cache_example_view(request):
     print(f'Apple: {value}')
     print(f'Mango: {mangos}')
 
-    # Delete a cache value (if needed)
     # cache.delete('orange')
 
     return HttpResponse(f'apple value: {value}\nmango value: {mangos}')
