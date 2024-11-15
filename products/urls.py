47dae1f42df9120256f140dceef5b00d6d364ca6
Praseesh <praseeshprasee@gmail.com>
2024-11-15 21:35:16 +0530

    checkout urls

--- products/urls.py
+++ products/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 from .views import (
-    CartView,ProductListAndFilterAPIView,CheckoutAPIView
+    CartView, # ProductListAndFilterAPIView,CheckoutAPIView
 )
 urlpatterns = [
     path('cart/', CartView.as_view(), name='cart'),
-    path('products-list-filter/', ProductListAndFilterAPIView.as_view(), name='product-list'),
-    path('checkout/', CheckoutAPIView.as_view(), name='checkout'),
+    # path('products-list-filter/', ProductListAndFilterAPIView.as_view(), name='product-list'),
+    # path('checkout/', CheckoutAPIView.as_view(), name='checkout'),
     
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
