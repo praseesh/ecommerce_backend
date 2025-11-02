ae5bebe6d7a147646a85e64a0f971435cb9b6c1d
Praseesh <150511158+praseesh@users.noreply.github.com>
2025-11-02 19:43:05 +0530

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,5 +1,4 @@
 INSERT INTO product (name, description, brand, price, rating, number_of_reviews, sold_count, stock_quantity, created_at, updated_at, category_id)
 VALUES 
 ('iPhone 14', 'The latest iPhone with advanced camera and performance', 'Apple', 109999.99, 0.0, 0, 0, 25, NOW(), NOW(), 1),
-('Noise Cancelling Headphones', 'High-quality wireless headphones with noise cancellation', 'Bose', 19999.99, 0.0, 0, 0, 50, NOW(), NOW(), 1),
-('Fitness Tracker', 'T
+('Noise Cancelling Headphones', 'High-quality wireless headphones with noise cancellation', 'Bose', 19999.99, 0.0, 0, 0, 50, NOW(), NOW(), 1);
